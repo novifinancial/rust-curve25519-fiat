25032824f44a31867cf56deb2693bec7eba6f229
François Garillot <francois@garillot.net>
2019-10-14 13:24:13 -0700

    Build maintenance
    - remove unarchivable file
    - stop using commands that assume presence of a git repo in build

--- curve25519-fiat/build.rs
+++ curve25519-fiat/build.rs
@@ -43,25 +43,18 @@ fn main() {
     // this folder
     let basedir = PathBuf::from(&get!("CARGO_MANIFEST_DIR"));
     let src_dir = basedir.join("src");
-    // println!("Dir is: {:?}", basedir);
+    println!("Dir is: {:?}", basedir);
     // the fiat-crypto submodule
     let fiat_crypto = basedir.join("external").join("fiat-crypto");
 
-    // get some data
-    run("git", |command| {
-        command
-            .arg("submodule")
-            .arg("update")
-            .arg("--init")
-            .arg("--recursive")
-            .arg("--checkout")
-    });
-    let cond = if let Ok(mut file) = fs::File::open(
-        &fiat_crypto
-            .join("fiat-rust")
-            .join("src")
-            .join("curve25519_64.rs"),
-    ) {
+    // this ised to do a submodule update, see
+    // https://github.com/google/shaderc-rs/issues/15 for why it does not any
+    // more
+    let src_path = fiat_crypto
+        .join("fiat-rust")
+        .join("src")
+        .join("curve25519_64.rs");
+    let cond = if let Ok(mut file) = fs::File::open(src_path) {
         let mut sha256 = Sha256::new();
         io::copy(&mut file, &mut sha256).expect("failed to copy file");
         let hash = sha256.result();
@@ -69,24 +62,43 @@ fn main() {
     } else {
         false
     };
+    // Go to the fiat directory
+    if !cond && !fiat_crypto.join(".git").exists() {
+        std::fs::remove_dir_all(&fiat_crypto).expect("could not remove fiat-crypto");
+        std::fs::create_dir_all(&basedir.join("external"))
+            .expect("could not create external subdirectory");
+        assert!(env::set_current_dir(&basedir.join("external")).is_ok());
+
+        run("git", |command| {
+            command
+                .arg("clone")
+                .arg("--recurse-submodules")
+                .arg("https://github.com/mit-plv/fiat-crypto.git")
+                .arg("fiat-crypto")
+        })
+    }
+    assert!(env::set_current_dir(&fiat_crypto).is_ok());
     if !cond {
-        // Go to the fiat directory
-        assert!(env::set_current_dir(&fiat_crypto).is_ok());
         // Checkout a particular dalek commit
         run("git", |command| command.arg("checkout").arg(FIAT_HASH));
     }
+    let long_file = fiat_crypto.join("coqprime").join("src").join("Coqprime").join("examples").join("prime216656403549020227250327256032933021325435259861468456540459488823774358486649614451547405419273433458932168893949521787.v");
+    let diag = format!("could not remove unarchivable file at {:?}", &long_file);
+    if long_file.exists() {
+        std::fs::remove_file(long_file).expect(&diag[..]);
+    };
+
     // Go to the base directory
     assert!(env::set_current_dir(&basedir).is_ok());
 
-    // move the curve25519_64 file from fiat to src/
-    run("cp", |command| {
-        command
-            .arg(
-                fiat_crypto
-                    .join("fiat-rust")
-                    .join("src")
-                    .join("curve25519_64.rs"),
-            )
-            .arg(&src_dir)
-    });
+    // copy the curve25519_64 file from fiat to src/
+    let origin_file = fiat_crypto
+        .join("fiat-rust")
+        .join("src")
+        .join("curve25519_64.rs");
+    let diag = format!(
+        "could not copy source file from {:?} to {:?}",
+        &origin_file, &src_dir,
+    );
+    std::fs::copy(origin_file, &src_dir.join("curve25519_64.rs")).expect(&diag[..]);
 }
